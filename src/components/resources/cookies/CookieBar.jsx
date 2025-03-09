9bc5a96ad1aece685a88f365cf21c8112b1f732a
rammelion <61567322+rammelion@users.noreply.github.com>
2025-03-09 18:39:46 +0100

    v 0.41. gtag for consent banner added step2

--- src/components/resources/cookies/CookieBar.jsx
+++ src/components/resources/cookies/CookieBar.jsx
@@ -7,6 +7,8 @@ export default function CookieBar() {
 
     const [isCookieSet, setCookie] = useState(Cookies.get("cookieConsent"));
 
+    const googleCookie = '_ga_DR8ZC0N9JR';
+
     const initCookieBar = () => {
         if (isCookieSet != null){
             return "hide-cookiebar";
@@ -26,6 +28,25 @@ export default function CookieBar() {
     const handleAcceptCookies = () => {
         Cookies.set("cookieConsent", true);
         setCookie(true);
+        window.dataLayer = window.dataLayer || [];
+        localStorage.setItem("consentGranted", "true");
+        function gtag() { window.dataLayer.push(arguments); }
+    
+        gtag('consent', 'update', {
+            ad_user_data: 'granted',
+            ad_personalization: 'granted',
+            ad_storage: 'granted',
+            analytics_storage: 'granted'
+        });
+        
+        
+          // Load gtag.js script.
+          var gtagScript = document.createElement('script');
+          gtagScript.async = true;
+          gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-DR8ZC0N9JR';
+        
+          var firstScript = document.getElementsByTagName('script')[0];
+          firstScript.parentNode.insertBefore(gtagScript,firstScript);
         hideCookieBar();
     };
 
