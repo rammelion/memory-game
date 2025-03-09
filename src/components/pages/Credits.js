f3af14e84974bc881f33f13538cd6af4f581f608
rammelion <61567322+rammelion@users.noreply.github.com>
2025-03-09 10:01:26 +0100

    v0.311, first sectin of credits page

---
+++ src/components/pages/Credits.js
@@ -0,0 +1,15 @@
+export default function Credits(props) {
+    return (
+        <div>
+            <h1>Credits</h1>
+            <p>
+                Cookie consent module is based on the idea of 
+                <a href="https://www.telerik.com/blogs/author/hassan-djirdeh"target="_blank">Hassan Djirdeh</a>
+                described in his 
+                <a href="https://www.telerik.com/blogs/react-basics-how-to-use-cookies">React Basics: How to Use Cookies in React</a>
+                 article at 
+                 <a href="https://www.telerik.com/blogs" target="_blank">Telerik Blogs</a>, October 24, 2023.
+            </p>
+        </div>
+    )
+}
